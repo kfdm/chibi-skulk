9224f887bcb600a437980c874560181230d24d75
Paul Traylor <paul@wegame.com>
2010-08-20 10:56:01 -0700

    Remove debug line

--- medic.pl
+++ medic.pl
@@ -27,7 +27,6 @@ Irssi::settings_add_str($IRSSI{'name'}, 'medic_alert', '');
 sub sig_public {
 	#"message public", SERVER_REC, char *msg, char *nick, char *address, char *target
 	my ($server, $msg, $nick, $address, $target) = @_;
-	print 'sig_message_public';
 	
 	# Only process if we actually have channels set
 	# and our watched channel matches the target
