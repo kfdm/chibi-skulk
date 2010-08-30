2caaae83516471eb01270c667f050fa8ecb0f186
Paul Traylor <paul@wegame.com>
2010-08-29 17:52:01 -0700

    Use the cs_watch var to only watch certain channels

--- rules.pl
+++ rules.pl
@@ -14,9 +14,22 @@ $VERSION = "1.00";
 Irssi::settings_add_str($IRSSI{'name'}, 'cs_rules_keyword', '^!rules');
 Irssi::settings_add_str($IRSSI{'name'}, 'cs_rules_url', '');
 
+sub cmd_medic {
+	Irssi::print('%G>>%n Rules can be configured with these settings:');
+	Irssi::print('%G>>%n cs_watch         : This is the channel we watch for rules events.');
+	Irssi::print('%G>>%n cs_rules_keyword : This is the regex we use to look for rules events.');
+	Irssi::print('%G>>%n cs_rules_url     : This is the URL we send.');
+}
+
 sub sig_public {
 	#"message public", SERVER_REC, char *msg, char *nick, char *address, char *target
 	my ($server, $msg, $nick, $address, $target) = @_;
+	
+	# Only process if we actually have channels set
+	# and our watched channel matches the target
+	my $watch = Irssi::settings_get_str('cs_watch');
+	return unless $watch ne '';
+	return unless $target eq $watch;
 	
 	# Make sure we have a keyword string
 	my $keyword = Irssi::settings_get_str('cs_rules_keyword');
@@ -32,5 +45,6 @@ sub sig_public {
 }
 
 Irssi::signal_add('message public', 'sig_public');
+Irssi::command_bind('rules', 'cmd_medic');
 
 Irssi::print('%G>>%n '.$IRSSI{name}.' '.$VERSION.' loaded');
